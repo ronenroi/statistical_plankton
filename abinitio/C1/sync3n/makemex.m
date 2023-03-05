52225f056778b830b58d82474587cce68b181aa0
ronenroi <60962549+ronenroi@users.noreply.github.com>
2023-03-05 21:29:39 +0200

    Add files via upload

---
+++ abinitio/C1/sync3n/makemex.m
@@ -0,0 +1,4 @@
+mex -O CFLAGS="\$CFLAGS -std=c99" pthreads/signs_times_v_mex.c 
+mex -O CFLAGS="\$CFLAGS -std=c99" pthreads/triangles_scores_mex.c
+mex -O CFLAGS="\$CFLAGS -std=c99" pthreads/pairs_probabilities_mex.c
+
